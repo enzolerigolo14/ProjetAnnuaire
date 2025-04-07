5c24c1cdc5350913e6b36abd0a2a7cef04b11765
enzo <enzo@gmail.com>
2025-04-07 17:00:04 +0200

    modif et regle de bug

--- client/script/connexion.js
+++ client/script/connexion.js
@@ -1,7 +1,10 @@
 const modal = document.getElementById("connexion-modal");
 const closeButton = document.querySelector(".close-button");
 const loginForm = document.getElementById("login-form");
+const username = document.getElementById("username").value;
 
+localStorage.setItem("isLoggedIn", "true");
+localStorage.setItem("username", username);
 
 closeButton.addEventListener("click", function () {
   modal.style.display = "none";
@@ -12,6 +15,12 @@ loginForm.addEventListener("submit", function (event) {
   event.preventDefault(); 
   window.location.href = "/client/src/pageaccueil.html";
 });
-const username = document.getElementById("username").value;
-  localStorage.setItem("isLoggedIn", "true");
-  localStorage.setItem("username", username);
+
+
+function validerNomUtilisateur(input) {
+  let cleaned = input.value.replace(/[^a-zA-ZÀ-ÿ\s\-']/g, '');
+  //const onlyDigits = cleaned.replace(/\D/g, ''); 
+  input.value = cleaned;
+}
+  
+  
